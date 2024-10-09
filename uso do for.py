1b09176e40e5b2f6892358990a0eea7126e62942
LuanJywago <luanjywago2015@gmail.com>
2024-10-09 08:51:07 -0300

    Update uso do for.py

--- uso do for.py
+++ uso do for.py
@@ -1,3 +1,4 @@
+## for com contador in range
 contador = 0
 
 for numero in range(11):
@@ -5,27 +6,33 @@ for numero in range(11):
     print("Numeros naturais inteiros na vertical: ", numero)
 print (contador)
 
+## for com lista de numeros
 numeros = ['1', '2', '3', '5', '6', '7', '8', '9', '10']
 for numeros in numeros:
     print (numeros, end=" ")
 
+## for com numeros na horizontal
 for numero in range(0, 11, 5):
     print (numero, end= ', ')
 
+## for com if para finalizar com ponto final
 for numero in range(0, 10, 1):
     print(numero, end=', ')
     if numero == 9:
         print("10.")
 
+#for com numero para dar valores apenas pares
 for numero in range(0, 14, 1):
     if numero % 2 == 0:
         print(numero, end=', ')
         if numero == 12:
             print("14.")
 
+#for inicial dos projetos
 for numero in range(7, -1, -1):
     print(numero)
 
-numeros = list(range(8))  # Cria uma lista de 0 a 7
+# Cria uma lista de 0 a 7
+numeros = list(range(8))
 for numero in reversed(numeros):
     print(numero)
